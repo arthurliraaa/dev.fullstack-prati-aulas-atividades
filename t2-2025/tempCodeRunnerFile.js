0587bec0be1142260bf4580710e1f8a8a61c432a
Arthur Lira <arthurlira1603@gmail.com>
2025-04-22 20:35:24 -0300

    .

---
+++ t2-2025/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+
+    resultado = 'Indefinido';
+}
+
+if (resultado !== 'Indefinido') {
+    console.log('O resultado é: ' + resultado);
+}
+
+console.log('O resultado é: ' + resultado);
+
+console.log('Obrigado por usar a calculadora!');
+console.log('Até a próxima!');
