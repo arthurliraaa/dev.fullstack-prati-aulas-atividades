ac4e770310f2b2d6069b33595371a30e616d1f25
Arthur Lira <arthurlira1603@gmail.com>
2025-04-29 20:17:42 -0300

    Create exercises.js

---
+++ t2-2025/exercises.js
@@ -0,0 +1,5 @@
+let percentualReprovadosTurmaC = 10
+let percentualReprovadosTurmaD = 85
+
+let quantidadeAlunosReprovadosTurmaC = (percentualReprovadosTurmaC *60)/100
+let quanbAprovadosTurmaD = (percentualReprovadosTurmaD * 20)/100
